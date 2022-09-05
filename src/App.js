a144ad94f421ea7fd9f5cce944c26e231a331ca8
Lennart Hartrumpf <lennart.hartrumpf@gmx.net>
2022-09-05 16:54:58 +0200

    Webvitals error fixed axiso request added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import "./App.css";
+import React from "react";
+import axios from "axios";
 
 function App() {
+  axios
+    .get(
+      `https://preview.contentful.com/spaces/6did8bhz0z37/environments/master/entries?access_token=kGj7JMivsAvCApuLy8ILXUYSq0pjNUmorIhF1-FztxA`
+    )
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => console.log(err));
+
   return <div className="App"></div>;
 }
 
