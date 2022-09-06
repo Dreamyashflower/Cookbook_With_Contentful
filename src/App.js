7613d279ed8263d4692728308d169089cc5344ae
Lennart Hartrumpf <lennart.hartrumpf@gmx.net>
2022-09-06 17:32:25 +0200

    fixed useState issue {} => [] added conditional rendering

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Recipes from "./components/Recipes";
 function App() {
-  const [assets, setAssets] = useState({});
+  const [assets, setAssets] = useState([]);
   useEffect(() => {
     axios
       .get(
         `https://preview.contentful.com/spaces/6did8bhz0z37/environments/master/entries?access_token=kGj7JMivsAvCApuLy8ILXUYSq0pjNUmorIhF1-FztxA`
       )
       .then((response) => {
+        console.log(response.data.items);
         setAssets(response.data.items);
       })
       .catch((err) => console.log(err));
   }, []);
   return (
     <div className="App">
-      {assets.map((recipes) => {
-        return <Recipes recipe={recipes} key={recipes.sys.id} />;
-      })}
+      {assets &&
+        assets.map((recipes) => {
+          return <Recipes recipe={recipes} key={recipes.sys.id} />;
+        })}
     </div>
   );
 }
